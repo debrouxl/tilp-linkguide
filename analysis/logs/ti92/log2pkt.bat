a814b185575924b195fc62fa81ec6762b8c9dffc
Romain Liévin <roms@lievin.net>
2005-07-05 06:28:21 +0000

    Added informations about 'Deleting Variable' on TI83+/TI84+.
    git-svn-id: http://svn.tilp.info/repos/tilp/linkguide/trunk@1314 11ccfa89-a3ba-0310-a1af-c6a367506c11

---
+++ analysis/logs/ti92/log2pkt.bat
@@ -0,0 +1,13 @@
+log2pkt.exe create_folder
+log2pkt.exe delete_var_x
+log2pkt.exe device_info
+log2pkt.exe dirlist
+log2pkt.exe recv_backup
+log2pkt.exe recv_var_x
+log2pkt.exe recv_var_y
+log2pkt.exe recv_vars
+log2pkt.exe screenshot
+log2pkt.exe send_backup
+log2pkt.exe send_var_x
+log2pkt.exe send_var_y
+log2pkt.exe send_vars
