dd8664fcf52c10217810097686b25aa452aeea0c
Romain Liévin <roms@lievin.net>
2005-07-05 08:24:57 +0000

    Added remote control section for TI73/TI83/TI83+/TI84+.
    git-svn-id: http://svn.tilp.info/repos/tilp/linkguide/trunk@1320 11ccfa89-a3ba-0310-a1af-c6a367506c11

---
+++ analysis/logs/ti83/log2pkt.bat
@@ -0,0 +1,12 @@
+log2pkt.exe delete_var_a
+log2pkt.exe device_info
+log2pkt.exe dirlist
+log2pkt.exe recv_backup
+log2pkt.exe recv_var_a
+log2pkt.exe recv_var_b
+log2pkt.exe recv_vars
+log2pkt.exe screenshot
+log2pkt.exe send_backup
+log2pkt.exe send_var_a
+log2pkt.exe send_var_b
+log2pkt.exe send_vars
