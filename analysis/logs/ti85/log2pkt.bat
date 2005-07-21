a651a36719fbdbc05f1cdc9459f16d2dfc6cfc2c
Romain Liévin <roms@lievin.net>
2005-07-21 13:53:27 +0000

    Added TI85 log files.
    git-svn-id: http://svn.tilp.info/repos/tilp/linkguide/trunk@1408 11ccfa89-a3ba-0310-a1af-c6a367506c11

---
+++ analysis/logs/ti85/log2pkt.bat
@@ -0,0 +1,11 @@
+log2pkt.exe recv_backup
+log2pkt.exe recv_var_a
+log2pkt.exe recv_var_b
+log2pkt.exe recv_vars
+log2pkt.exe screenshot
+log2pkt.exe send_backup
+log2pkt.exe send_var_a
+log2pkt.exe send_var_a_skip
+log2pkt.exe send_var_b
+log2pkt.exe send_var_b_exit
+log2pkt.exe send_vars
