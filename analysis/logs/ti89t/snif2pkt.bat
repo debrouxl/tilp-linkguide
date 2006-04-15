47ba1e7872f1650c894e550c018f82dda97d16a1
Romain Liévin <roms@lievin.net>
2006-04-15 06:40:09 +0000

    Updated scripts.
    git-svn-id: http://svn.tilp.info/repos/tilp/linkguide/trunk@2199 11ccfa89-a3ba-0310-a1af-c6a367506c11

---
+++ analysis/logs/ti89t/snif2pkt.bat
@@ -0,0 +1,18 @@
+snif2pkt device_info
+
+snif2pkt clock_get
+snif2pkt clock_set
+
+snif2pkt delete_app
+snif2pkt delete_var_a
+snif2pkt delete_var_b
+
+snif2pkt dirlist
+
+snif2pkt recv_var_a
+snif2pkt recv_var_b
+
+snif2pkt send_var_a
+snif2pkt send_var_b
+
+snif2pkt screenshot
