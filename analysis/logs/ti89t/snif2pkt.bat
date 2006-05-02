5c00e3106f49ebeef894732df7915920d0c50403
Romain Liévin <roms@lievin.net>
2006-05-02 17:52:37 +0000

    Added Titanium Direct Usb logs.
    git-svn-id: http://svn.tilp.info/repos/tilp/linkguide/trunk@2243 11ccfa89-a3ba-0310-a1af-c6a367506c11

--- analysis/logs/ti89t/snif2pkt.bat
+++ analysis/logs/ti89t/snif2pkt.bat
@@ -1,18 +1,29 @@
-snif2pkt device_info
-
 snif2pkt clock_get
 snif2pkt clock_set
 
 snif2pkt delete_app
-snif2pkt delete_var_a
-snif2pkt delete_var_b
+snif2pkt delete_var_x
+snif2pkt delete_var_y
+snif2pkt delete_var_z
+
+snif2pkt device_info
 
 snif2pkt dirlist
 
-snif2pkt recv_var_a
-snif2pkt recv_var_b
+snif2pkt recv_flash_app
 
-snif2pkt send_var_a
-snif2pkt send_var_b
+snif2pkt recv_var_x
+snif2pkt recv_var_y
+snif2pkt recv_vars
 
 snif2pkt screenshot
+
+rem snif2pkt send_cert
+snif2pkt send_flash_app
+snif2pkt send_flash_os
+
+snif2pkt send_var_x
+snif2pkt send_var_y
+snif2pkt send_var_z
+
+rem snif2pkt check_updates
