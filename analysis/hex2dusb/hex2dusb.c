2fac036c7377114e987c742c39ce7880f5e73269
Romain Liévin <roms@lievin.net>
2007-10-03 00:52:48 +0000

    hex2dusb has to be written...
    git-svn-id: http://svn.tilp.info/repos/tilp/linkguide/trunk@3850 11ccfa89-a3ba-0310-a1af-c6a367506c11

--- analysis/hex2dusb/hex2dusb.c
+++ analysis/hex2dusb/hex2dusb.c
@@ -16,6 +16,9 @@
  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
+
+/* !!! Not working yet !!! */
+
 #include <conio.h>
 #include <ctype.h>
 #include <stdio.h>
