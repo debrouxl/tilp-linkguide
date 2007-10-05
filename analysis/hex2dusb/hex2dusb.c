60c9f01ca8ee540691441b7598fff3cbf128f125
Romain Liévin <roms@lievin.net>
2007-10-05 19:54:10 +0000

    Program written and tested. Now, log2hex + hex2dusb supersedes log2dusb.
    git-svn-id: http://svn.tilp.info/repos/tilp/linkguide/trunk@3866 11ccfa89-a3ba-0310-a1af-c6a367506c11

--- analysis/hex2dusb/hex2dusb.c
+++ analysis/hex2dusb/hex2dusb.c
@@ -40,6 +40,8 @@ typedef unsigned char	uint8_t;
 typedef unsigned short	uint16_t;
 typedef unsigned long	uint32_t;
 
+/* */
+
 typedef struct
 {
 	uint8_t		type;
@@ -88,6 +90,8 @@ static const Opcode opcodes[] =
 	{ 0xee00, "Error"},
 	{ 0 },
 };
+
+/* */
 
 int is_a_packet(uint8_t id)
 {
@@ -147,65 +151,10 @@ const char* ep_way(int ep)
 {
 	if(ep == 0x01) return "TI>PC";
 	else if(ep == 0x02) return "PC>TI";
-	else return "??>??";
+	else return "XX>XX";
 }
 
-typedef struct
-{
-	uint32_t	size;
-	uint8_t		type;
-} PktHdr;
-
-typedef struct
-{
-	uint32_t	size;
-	uint16_t	opcode;
-} DataHdr;
-
-typedef struct
-{
-	uint8_t		ep;			// endpoint aka direction (0x81 = IN, 0x02 = OUT)
-	uint32_t	size;		// size of packet
-	uint8_t		data[2048];	// data
-	uint32_t	len;		// length of data
-
-	int			frag;		// fragmented
-} Urb;
-
-#define WIDTH	12
-
-int fill_buf(FILE *f, uint8_t data, int flush)
-{
-	static char buf[WIDTH];
-	static unsigned int cnt = 0;
-	unsigned int i, j;
-
-	if(!flush)
-		buf[cnt++] = data;
-
-	if((cnt >= WIDTH) || flush)
-	{
-		//printf(".");
-		fprintf(f, "    ");
-		for(i = 0; i < cnt; i++)
-			fprintf(f, "%02X ", 0xff & buf[i]);
-
-		if(flush)
-			for(j = i; j < WIDTH; j++)
-				fprintf(f, "   ");
-
-		fprintf(f, "| ");
-		for(i = 0; i < cnt; i++)
-			fprintf(f, "%c", isalnum(buf[i]) ? buf[i] : '.');
-
-		fprintf(f, "\n");
-		cnt = 0;
-	}
-
-	return 0;
-}
-
-Urb** urbs;
+/* */
 
 int add_pkt_type(uint8_t* array, uint8_t type, int *count)
 {
@@ -235,309 +184,152 @@ int add_data_code(uint16_t* array, uint16_t code, int *count)
 	return i;
 }
 
-/*
-  Format of data: 12 hexadecimal numbers with spaces
-*/
-int pkt_write(const char *filename, int nurbs)
+/* */
+
+static FILE *hex = NULL;
+static FILE *log = NULL;
+
+int hex_read(unsigned char *data)
 {
-	FILE *fo;
-	int i;
-	unsigned int j;
+	static char line[256];
+	static int idx = 0;
+	int ret;
 
-	uint8_t pkt_type_found[256] = { 0 };
-	uint16_t data_code_found[256] = { 0 };
-	int ptf=0, dcf=0;
+	if(feof(hex))
+		return -1;
 
-	fo = fopen(filename, "wt");
-    if(fo == NULL)
-    {
-        fprintf(stderr, "Unable to open this file: %s\n", filename);
-        return -1;
-    }
+	ret = fscanf(hex, "%02X", data);
+	if(ret < 1)
+		return -1;
+	fgetc(hex);
+	idx++;
 
-#if 0
-
-	for(ptr = urbs; *ptr; ptr++)
+	if(idx >= 16)
 	{
-		Urb *u = *ptr;
+		int i;
 
-		fprintf(fo, "%i: (%3i) ", u->ep, u->size);
-
-		for(i = 0; i <u->size; i++)
-			fprintf(fo, "%02X ", u->data[i]);
-		fprintf(fo, "\n");
+		idx = 0;
+		for(i = 0; (i < 67-49) && !feof(hex); i++)
+			fgetc(hex);
 	}
-
-#else
-    
-    fprintf(fo, "TI packet decompiler for D-USB, version 1.0\n");
-
-	// process data
-	for(i = 0; i < nurbs; i++)
-    {
-		Urb *u = urbs[i];
-		uint32_t pkt_size = 0;
-		uint8_t pkt_type = 0;
-		uint32_t data_size = 0;
-		uint16_t data_code = 0;
-		static int concat = 0;
-		static uint32_t buf_size = 0x3ff;
-
-		pkt_size = u->data[3] | (u->data[2] << 8) | (u->data[1] << 16) | (u->data[0] << 24);
-		pkt_type = u->data[4];
-
-		fprintf(fo, "%08x (%02x) ", pkt_size, pkt_type);
-		fprintf(fo, "\t\t\t\t\t\t\t");
-		fprintf(fo, "| %s: %s %s\n", ep_way(u->ep), name_of_packet(pkt_type), u->frag ? "(fragmented)" : "");
-
-		add_pkt_type(pkt_type_found, pkt_type, &ptf);
-
-		if((pkt_type == 1) || pkt_type == 2)
-		{
-			data_size = u->data[8] | (u->data[7] << 8) | (u->data[6] << 16) | (u->data[5] << 24);
-			if(pkt_type == 2)
-				buf_size = data_size;
-		}
-		else if(pkt_type == 5)
-			data_size = u->data[6] | (u->data[5] << 8);
-		
-		if(is_a_packet_with_data_header(pkt_type) && !concat)
-		{
-			//if(pkt_type == 0x03 && (pkt_size == 0xFA || pkt_size == 0x3FF))
-			if((pkt_type == 0x03) && (pkt_size == buf_size))
-				concat = 1;
-
-			data_size = u->data[8] | (u->data[7] << 8) | (u->data[6] << 16) | (u->data[5] << 24);
-			data_code = u->data[10] | (u->data[9] << 8);
-			
-			add_data_code(data_code_found, data_code, &dcf);
-		
-			fprintf(fo, "\t%08x {%04x} ", data_size, data_code);
-			fprintf(fo, "\t\t\t\t\t");
-			fprintf(fo, "| %s: %s\n", "CMD", name_of_data(data_code));
-
-			for(j = 0; j < data_size && j < pkt_size-5; j++)
-			{
-				fill_buf(fo, u->data[11 + j], 0);
-			}
-			fill_buf(fo, 0, !0);
-		}
-		else if(concat && ((pkt_type == 0x03) || (pkt_type == 0x04)))
-		{
-			if(pkt_type == 0x04)
-				concat = 0;
-
-			for(j = 0; j < pkt_size; j++)
-			{
-				fill_buf(fo, u->data[5 + j], 0);
-			}
-			fill_buf(fo, 0, !0);
-		}
-		else
-		{
-			if(pkt_type == 0x05)
-				fprintf(fo, "\t[%04x]\t", data_size);
-			else
-				fprintf(fo, "\t[%08x]", data_size);
-			fprintf(fo, "\t\t\t\t\t\t\t");
-			fprintf(fo, "|\n");
-		}
-
-		fprintf(fo, "\n");
-	}
-
-	fprintf(fo, "() Packet types found: ");
-	for(i = 0; i < ptf; i++) fprintf(fo, "%02x ", pkt_type_found[i]);
-	fprintf(fo, "\n");
-	fprintf(fo, "{} Data codes found: ");
-	for(i = 0; i < dcf; i++) fprintf(fo, "%04x ", data_code_found[i]);
-	fprintf(fo, "\n");
-
-#endif
-
-	fclose(fo);
-	return 0;
-}
-
-//"     00000000: 00 00 00 0a 04 00 00 00 04 00 12 00 00 07 d0"
-int read_line(const char* str, uint8_t* data)
-{
-	const char *s = str + strlen(TOKEN5);
-	unsigned int i;
-
-	for(i = 0; (i <= strlen(s) / 3) && (i < 16); i++)
-		sscanf(&s[3*i], "%02x", &data[i]);
-
-	return i;
-}
-
-// parse file on a per-urb basis
-static Urb* load_urb(FILE *fi)
-{
-	int found = 0;
-	char str[1024];
-	unsigned int ep = -1;
-	unsigned int nbytes;
-	Urb *urb;
-
-	if(feof(fi))
-		return NULL;
-	
-	urb = calloc(1, sizeof(Urb));
-
-	for(nbytes = 0; found < 2 && !feof(fi);)
-	{
-		fgets(str, sizeof(str), fi);
-
-		if(!strncmp(str, TOKEN1, strlen(TOKEN1)))
-		{
-			found = 1;
-			fgets(str, sizeof(str), fi);
-			sscanf(str + strlen(TOKEN4), "0x%08x]", &ep);
-		}
-
-		if(found && !strncmp(str, TOKEN2, strlen(TOKEN2)))
-		{
-			uint8_t line[20], data[2048];
-			unsigned int size;
-
-			memset(data, 0, sizeof(data));
-
-			for(fgets(str, sizeof(str), fi);
-				strncmp(str, TOKEN3, strlen(TOKEN3)); 
-				fgets(str, sizeof(str), fi))
-			{
-				str[strlen(str)-1] = 0;
-
-				size = read_line(str, line);
-				memcpy(&data[nbytes], line, size);
-				nbytes += size;
-
-				//printf("%s (%i)\n", str, size);
-				//printf(".");
-			}
-
-			if(!nbytes)
-				continue;
-
-			urb->size = (data[3] | (data[2] << 8) | (data[1] << 16) | (data[0] << 24)) + 5;
-
-			if(nbytes > 0)
-			{
-				urb->ep = ep & ~0x80;
-				urb->len = nbytes;
-				memcpy(urb->data, data, urb->len);
-			}
-
-			found = 2;
-		}
-	}
-
-	return urb;
-}
-
-/*
-	Packet can have up to 255 bytes and we often have a TI packet per URB but
-	this is not always the case (especially with TiConnect&Titanium).
- */
-int snif_read(const char* filename, int* nurbs)
-{
-    FILE *fi;
-	char str[1024];
-	int ret = 0;
-	int found = 0;
-	int nlines = 0;
-	int i = 0;
-	int j = 0;
-	int k = 0;
-
-	Urb *prev = NULL;
-	int concat = 0;
-   
-    // open file
-	fi = fopen(filename, "rt");
-    if(fi == NULL)
-    {
-        fprintf(stderr, "Unable to open this file: %s\n", filename);
-        return -1;
-    }
-
-	// first pass: count data
-	while(!feof(fi))
-	{
-		fgets(str, sizeof(str), fi);
-
-		if(!strncmp(str, TOKEN1, strlen(TOKEN1)))
-			found = 1;
-
-		if(found && !strncmp(str, TOKEN2, strlen(TOKEN2)))
-		{
-			for(fgets(str, sizeof(str), fi);
-				strncmp(str, TOKEN3, strlen(TOKEN3)); 
-				fgets(str, sizeof(str), fi))
-			{
-				nlines++;
-			}
-			found = 0;
-		}
-
-	}
-    
-	// allocate a NULL-terminated array of Urb's
-    urbs = (Urb **)calloc(nlines + 1, sizeof(Urb *));
-    if(urbs == NULL)
-    {
-        fprintf(stderr, "calloc error.\n");
-        exit(-1);
-    }
-
-	fprintf(stdout, "Reading data:\n");
-
-	// second pass: load data
-    rewind(fi);
-	while(!feof(fi))
-	{
-		Urb *tmp, *urb;
-		
-		urb = load_urb(fi);
-		if(!urb) break;
-		printf(".");
-
-		// Big blocks of data on Titanium are fragmented. Re-assemble here
-		if(urb->len < urb->size)
-		{
-			urb->frag = 1;
-			i++;
-			//printf("!! <%08x-%08x> \n", urb->len, urb->size);
-			
-			do
-			{
-				tmp = load_urb(fi);
-				if(!tmp) break;
-				printf("*");
-				k++;
-				
-				memcpy(urb->data + urb->len, tmp->data, tmp->len);
-				urb->len += tmp->len;
-
-				free(tmp);
-			} while(urb->len < urb->size);
-
-			//printf("$ <%08x-%08x> \n", urb->len, urb->size);
-		}
-		//else
-			//printf(<%08x-%08x> \n", urb->len, urb->size);
-
-		urbs[j++] = urb;
-	}
-
- 	*nurbs = --j;
-	fclose(fi);
-
-	printf("\n");
-	fprintf(stdout, "Processed %i blocks into %i packets (%i packets were fragmented into %i blocks).\n", j+k, j, i, k);
     
 	return 0;
+}
+
+uint8_t pkt_type_found[256] = { 0 };
+uint16_t data_code_found[256] = { 0 };
+int ptf=0, dcf=0;
+
+int dusb_write(int dir, uint8_t data)
+{
+	static int array[20];
+  	static int i = 0;
+	static unsigned long state = 1;
+	static uint32_t raw_size;
+	static uint8_t raw_type;
+	static uint32_t vtl_size;
+	static uint16_t vtl_type;
+	static int cnt;
+	static int first = 1;
+
+  	if (log == NULL)
+    		return -1;
+
+	//printf("<%i %i> ", i, state);
+	array[i++ % 16] = data;
+
+	switch(state)	// Finite State Machine
+	{
+	case 1: break;
+	case 2: break;
+	case 3: break;
+	case 4: 
+		raw_size = (array[0] << 24) | (array[1] << 16) | (array[2] << 8) | (array[3] << 0);
+		fprintf(log, "%08x ", raw_size);
+		break;
+	case 5: 
+		raw_type = array[4];
+		fprintf(log, "(%02X) ", raw_type);
+
+		fprintf(log, "\t\t\t\t\t\t\t");
+		fprintf(log, "| %s: %s\n", ep_way(dir), name_of_packet(raw_type));
+		add_pkt_type(pkt_type_found, raw_type, &ptf);
+
+		break;
+	case 6: break;
+	case 7:
+		if(raw_type == 5)
+		{
+			uint16_t tmp = (array[5] << 8) | (array[6] << 0);
+			fprintf(log, "\t[%04x]\n", tmp);
+			state = 0;
+		}
+		break;
+	case 8: break;
+	case 9:
+		if(raw_type == 1 || raw_type == 2)
+		{
+			uint32_t tmp = (array[5] << 24) | (array[6] << 16) | (array[7] << 8) | (array[8] << 0);
+			fprintf(log, "\t[%08x]\n", tmp);
+			state = 0;
+		}
+		else if(first && ((raw_type == 3) || (raw_type == 4)))
+		{
+			vtl_size = (array[5] << 24) | (array[6] << 16) | (array[7] << 8) | (array[8] << 0);
+			fprintf(log, "\t%08x ", vtl_size);
+			cnt = 0;
+			first = (raw_type == 3) ? 0 : 1;
+			raw_size -= 6;
+		}
+		else if(!first && ((raw_type == 3) || (raw_type == 4)))
+		{
+			fprintf(log, "\t");
+			fprintf(log, "%02X %02X %02X ", array[5], array[6], array[7]);
+			cnt = 3;
+			raw_size -= 3;
+			first = (raw_type == 3) ? 0 : 1;
+
+			state = 12;
+			goto push;
+		}			
+		break;
+	case 10: break;
+	case 11:
+		vtl_type = (array[9] << 8) | (array[10] << 0);
+		fprintf(log, "{%04x}", vtl_type);
+		
+		fprintf(log, "\t\t\t\t\t\t");
+		fprintf(log, "| %s: %s\n\t\t", "CMD", name_of_data(vtl_type));
+		add_data_code(data_code_found, vtl_type, &dcf);
+
+		if(!vtl_size)
+		{
+			fprintf(log, "\n");
+			state = 0;
+		}
+		break;
+	default: push:
+		fprintf(log, "%02X ", data);
+
+		if(!(++cnt % 12))
+			fprintf(log, "\n\t\t");
+		
+		if(--raw_size == 0)
+		{
+			fprintf(log, "\n");
+			state = 0;
+		}
+		break;
+	}
+
+	if(state == 0)
+	{
+		fprintf(log, "\n");
+		i = 0;
+	}
+	state++;	
+
+  	return 0;
 }
 
 int main(int argc, char **argv)
@@ -545,26 +337,58 @@ int main(int argc, char **argv)
 	char* filename = argv[1];
 	char src_name[1024];
 	char dst_name[1024];
-	int nurbs;
+	unsigned char data;
+	int i;
 
 	if(argc < 2)
     {
-		fprintf(stderr, "Usage: log2pkt [file]\n");
+		fprintf(stderr, "Usage: hex2dusb [file]\n");
 		exit(0);
     }
 
 	strcpy(src_name, filename);
-    strcat(src_name, ".log");
+    strcat(src_name, ".hex");
 
 	strcpy(dst_name, filename);
     strcat(dst_name, ".pkt");
     
-	snif_read(src_name, &nurbs);
-	pkt_write(dst_name,  nurbs);
+	hex = fopen(src_name, "rt");
+    if(hex == NULL)
+    {
+        fprintf(stderr, "Unable to open this file: %s\n", src_name);
+        return -1;
+    }
 
-	free(urbs);
+	log = fopen(dst_name, "wt");
+	if(log == NULL)
+    {
+        fprintf(stderr, "Unable to open this file: %s\n", dst_name);
+        return -1;
+    }
 
-	//while(!kbhit());
+	{
+		char line[256];
+
+		fgets(line, sizeof(line), hex);
+		fgets(line, sizeof(line), hex);
+		fgets(line, sizeof(line), hex);
+	}
+
+	fprintf(log, "TI packet decompiler for D-USB, version 1.0\n");
+
+	while(hex_read(&data) != -1)
+	{
+		dusb_write(0, data);
+	}
+
+	fprintf(log, "() Packet types found: ");
+	for(i = 0; i < ptf; i++) fprintf(log, "%02x ", pkt_type_found[i]);
+	fprintf(log, "\n");
+	fprintf(log, "{} Data codes found: ");
+	for(i = 0; i < dcf; i++) fprintf(log, "%04x ", data_code_found[i]);
+	fprintf(log, "\n");
+
+	fclose(hex);
   
 	return 0;
 }
