48dd1278bdec3edb995f4f067810a38f490bb5f3
Romain Liévin <roms@lievin.net>
2007-11-11 14:13:47 +0000

    Use standard types.
    git-svn-id: http://svn.tilp.info/repos/tilp/linkguide/trunk@4017 11ccfa89-a3ba-0310-a1af-c6a367506c11

--- analysis/hex2dusb/hex2dusb.c
+++ analysis/hex2dusb/hex2dusb.c
@@ -22,6 +22,8 @@
 #include <string.h>
 #include <sys/stat.h>
 
+#include "stdints1.h"
+
 #define HEXDUMP_SIZE	12
 
 /*
@@ -33,12 +35,6 @@
 	|			  |    |			 |		 |								 |
 	| 00 00 00 10 | 04 | 00 00 00 0A | 00 01 | 00 03 00 01 00 00 00 00 07 D0 |	
 */
-
-typedef unsigned char	uint8_t;
-typedef unsigned short	uint16_t;
-typedef unsigned long	uint32_t;
-
-/* */
 
 typedef struct
 {
